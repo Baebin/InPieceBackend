3327439f1dd7baeede7653c0cc09cae35a145250
Baebin <baebine@naver.com>
2024-07-15 20:33:59 +0900

    Initial commit

---
+++ src/main/java/com/piebin/inpiece/InPieceApplication.java
@@ -0,0 +1,13 @@
+package com.piebin.inpiece;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InPieceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InPieceApplication.class, args);
+    }
+
+}
