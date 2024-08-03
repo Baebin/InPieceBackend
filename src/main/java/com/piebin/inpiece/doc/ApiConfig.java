32d35f54694ff506513aa86630ecc10fec7c02a6
Baebin <baebine@naver.com>
2024-08-03 17:56:55 +0900

    Api Doc Category Added

---
+++ src/main/java/com/piebin/inpiece/doc/ApiConfig.java
@@ -0,0 +1,57 @@
+package com.piebin.inpiece.doc;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(servers = {@Server(url = "/")})
+public class ApiConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(new Info()
+                        .title("InPiece Backend")
+                        .description("Spring Boot API")
+                        .version("v0.0.1"));
+    }
+
+    // API
+    @Bean
+    public GroupedOpenApi defaultAPI() {
+        return GroupedOpenApi.builder()
+                .group("Default API")
+                .pathsToMatch("/**")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi accountAPI() {
+        return GroupedOpenApi.builder()
+                .group("Account API")
+                .pathsToMatch("/api/account/**")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi contestAPI() {
+        return GroupedOpenApi.builder()
+                .group("Contest API")
+                .pathsToMatch("/api/contest/**")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi teamAPI() {
+        return GroupedOpenApi.builder()
+                .group("Team API")
+                .pathsToMatch("/api/team/**")
+                .build();
+    }
+}
