60543344354add060f47648d79643f69e5d86493
Baebin <63220297+Baebin@users.noreply.github.com>
2024-08-05 02:20:59 +0900

    War & Jar Setting

--- src/main/java/com/piebin/inpiece/InPieceApplication.java
+++ src/main/java/com/piebin/inpiece/InPieceApplication.java
@@ -1,12 +1,26 @@
 package com.piebin.inpiece;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.TimeZone;
 
 @EnableJpaAuditing
 @SpringBootApplication
-public class InPieceApplication {
+public class InPieceApplication extends SpringBootServletInitializer {
+    @PostConstruct
+    void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(InPieceApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(InPieceApplication.class, args);
